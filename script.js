b8b70e2da0154d5d134baf06cca51c2289c3c969
Ken Chen <54648555+KKennn@users.noreply.github.com>
2022-03-08 15:31:26 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -9,32 +9,56 @@ var questions = [
     "What acoustic details will you use to describe this memory?"
 ]
 
-
-var userData = [];
-userData.push(["test", "test"]);
-var saveData = document.getElementById("input");
-var thisId;
-var complete = false;
-
-function showDiv(groupId) {
-    document.querySelector('.ask-input').style.display = "block";
-    saveData.placeholder = "Log some memories …" + "\n" + "Inspiration: " + questions[parseInt(Math.random() * questions.length)];
-    thisId = groupId;
-    for (var i = 0; i < userData.length; i++) {
-        if (userData[i][1] == thisId) {
-            saveData.value = userData[i][0];
-            break;
-        }
-    }
+function generate() {
+    document.getElementById('page-2').style.display = 'none';
+    document.getElementById('page-3').style.display = 'block';
+    scroll(0, 0);
 }
+
+function goBack() {
+    document.getElementById('page-3').style.display = 'none';
+    document.getElementById('page-2').style.display = 'block';
+    scroll(0, 0);
+}
+
+
 
 let imgUpload = document.getElementById('file-upload'),
     totalFiles;
+
+let photoGroups = document.querySelector('.drop-targets');
+
+function newGroup(d) {
+    let photoGroup = document.createElement("div");
+    photoGroup.setAttribute('class', 'scroll-container box');
+    photoGroup.innerHTML = '<div class="gridscroll"></div>' +
+        '<div class="log-button" id="group' + (d + 1) + '" onclick="showDiv(this.id)">' +
+        '<svg height="96" width="96" class="write">' +
+        '<polygon points="30,0 96,0 96,96 0,96" />' +
+        '</svg>' +
+        '<p class="write-text">Log</p>' +
+        '<img src="media/log.svg" class="write-icon">' +
+        '</div>';
+
+    photoGroup.addEventListener('dragenter', dragEnter)
+    photoGroup.addEventListener('dragover', dragOver);
+    photoGroup.addEventListener('dragleave', dragLeave);
+    photoGroup.addEventListener('drop', drop);
+    photoGroups.appendChild(photoGroup);
+}
+
+// let addButton = document.querySelector(".add");
+// addButton.addEventListener('click', newGroup);
 
 imgUpload.addEventListener('change', previewImgs, false);
 
 function previewImgs(event) {
     totalFiles = imgUpload.files.length;
+
+    for (let p = 0; p <= totalFiles / 3; p++) {
+        console.log(p);
+        newGroup(p);
+    }
 
     for (let i = 0; i < totalFiles; i++) {
         let tile = document.createElement('div');
@@ -88,9 +112,11 @@ function previewImgs(event) {
     }
     document.getElementById('page-1').style.display = 'none';
     document.getElementById('page-2').style.display = 'block';
-
+    scroll(0, 0);
 
 }
+
+
 
 // for (let k = 1; k <= 15; k++) {
 //     var img = document.createElement('div');
@@ -105,16 +131,16 @@ function previewImgs(event) {
 // }
 
 // select the item element
-// const items = document.querySelectorAll('.item');
+let items = document.querySelectorAll('.item');
 
-// var i = 1;
+var i = 1;
 
 // attach the dragstart event handler
-// items.forEach(item => {
-//     item.setAttribute("id", i);
-//     item.addEventListener('dragstart', dragStart);
-//     i++;
-// })
+items.forEach(item => {
+    item.setAttribute("id", i);
+    item.addEventListener('dragstart', dragStart);
+    i++;
+})
 
 // handle the dragstart
 
@@ -127,14 +153,14 @@ function dragStart(e) {
     }, 0);
 }
 
-const boxes = document.querySelectorAll('.box');
+// let boxes = document.querySelectorAll('.box');
 
-boxes.forEach(box => {
-    box.addEventListener('dragenter', dragEnter)
-    box.addEventListener('dragover', dragOver);
-    box.addEventListener('dragleave', dragLeave);
-    box.addEventListener('drop', drop);
-});
+// boxes.forEach(box => {
+//     box.addEventListener('dragenter', dragEnter)
+//     box.addEventListener('dragover', dragOver);
+//     box.addEventListener('dragleave', dragLeave);
+//     box.addEventListener('drop', drop);
+// });
 
 function dragEnter(e) {
     e.preventDefault();
@@ -185,6 +211,23 @@ function drop(e) {
 //     }
 // })
 
+var userData = [];
+userData.push(["test", "test"]);
+var saveData = document.getElementById("input");
+var thisId;
+var complete = false;
+
+function showDiv(groupId) {
+    document.querySelector('.ask-input').style.display = "block";
+    saveData.placeholder = "Log some memories …" + "\n" + "Inspiration: " + questions[parseInt(Math.random() * questions.length)];
+    thisId = groupId;
+    for (var i = 0; i < userData.length; i++) {
+        if (userData[i][1] == thisId) {
+            saveData.value = userData[i][0];
+            break;
+        }
+    }
+}
 
 function hideDiv() {
     document.querySelector('.ask-input').style.display = "none";
@@ -210,7 +253,7 @@ function hideDiv() {
     var buttons = document.getElementsByClassName("write-text");
     for (var i = 0; i < buttons.length; i++) {
         complete = true;
-        if (buttons[i].innerHTML == "Log") {
+        if (buttons[i].innerHTML == "Log" && !buttons[i].parentElement.parentElement.classList.contains('hide')) {
             complete = false;
             break;
         }
@@ -224,78 +267,78 @@ function hideDiv() {
     }
 }
 
-// Get the modal
-var modal = document.getElementById("myModal");
+// // Get the modal
+// var modal = document.getElementById("myModal");
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+// // Get the button that opens the modal
+// var btn = document.getElementById("myBtn");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-    modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
-
-// Get the modal
-var writeModal = document.getElementById("writeModal");
-
-// Get the button that opens the modal
-var writeButton = document.getElementsByClassName("tile")[0];
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-writeButton.onclick = function() {
-    writeModal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    writeModal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        writeModal.style.display = "none";
-    }
-}
-
-for (var i = 0; i < totalFiles; i++) {
-    img = document.createElement('img');
-    img.src = URL.createObjectURL(event.target.files[i]);
-    img.classList.add('img-preview-thumb');
-    imgPreview.appendChild(img);
-}
-
-// Get the modal
-var input = document.querySelector(".ask-input");
-
-// Get the button that opens the modal
-var tile = document.querySelector(".tile");
-
-// Get the <span> element that closes the modal
+// // Get the <span> element that closes the modal
 // var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on the button, open the modal
-tile.onclick = function() {
-    input.style.display = "block";
-}
+// // When the user clicks on the button, open the modal
+// btn.onclick = function() {
+//     modal.style.display = "block";
+// }
+
+// // When the user clicks on <span> (x), close the modal
+// span.onclick = function() {
+//     modal.style.display = "none";
+// }
+
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//     if (event.target == modal) {
+//         modal.style.display = "none";
+//     }
+// }
+
+// // Get the modal
+// var writeModal = document.getElementById("writeModal");
+
+// // Get the button that opens the modal
+// var writeButton = document.getElementsByClassName("tile")[0];
+
+// // Get the <span> element that closes the modal
+// var span = document.getElementsByClassName("close")[0];
+
+// // When the user clicks on the button, open the modal
+// writeButton.onclick = function() {
+//     writeModal.style.display = "block";
+// }
+
+// // When the user clicks on <span> (x), close the modal
+// span.onclick = function() {
+//     writeModal.style.display = "none";
+// }
+
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//     if (event.target == modal) {
+//         writeModal.style.display = "none";
+//     }
+// }
+
+// for (var i = 0; i < totalFiles; i++) {
+//     img = document.createElement('img');
+//     img.src = URL.createObjectURL(event.target.files[i]);
+//     img.classList.add('img-preview-thumb');
+//     imgPreview.appendChild(img);
+// }
+
+// // Get the modal
+// var input = document.querySelector(".ask-input");
+
+// // Get the button that opens the modal
+// var tile = document.querySelector(".tile");
+
+// // Get the <span> element that closes the modal
+// // var span = document.getElementsByClassName("close")[0];
+
+// // When the user clicks on the button, open the modal
+// tile.onclick = function() {
+//     input.style.display = "block";
+// }
 
 // When the user clicks on <span> (x), close the modal
 // span.onclick = function() {
